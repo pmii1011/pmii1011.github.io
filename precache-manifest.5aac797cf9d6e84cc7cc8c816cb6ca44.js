9d458467037bb477a7188a9ee192f330286d4eb7
ikmalil birri <malikmal98@gmail.com>
2020-08-07 05:05:30 +0700

    update progress landing page"

--- precache-manifest.5aac797cf9d6e84cc7cc8c816cb6ca44.js
+++ precache-manifest.5aac797cf9d6e84cc7cc8c816cb6ca44.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "86a37ac18442e84c3096",
+    "revision": "09717ce1a0771fc5cbed",
     "url": "/css/app.6e5acdc6.css"
   },
   {
-    "revision": "86a37ac18442e84c3096",
-    "url": "/js/app.f831ce2f.js"
+    "revision": "09717ce1a0771fc5cbed",
+    "url": "/js/app.f65a4776.js"
   },
   {
     "revision": "e7cb5a525a66d322dd6c",
@@ -28,43 +28,43 @@ self.__precacheManifest = [
     "url": "/fonts/nucleo-icons.c1733565.eot"
   },
   {
-    "revision": "426439788ec5ba772cdf94057f6f4659",
-    "url": "/fonts/nucleo-icons.42643978.woff2"
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
   },
   {
     "revision": "f82ec6ba2dc4181db2af35c499462840",
     "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
   },
   {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
     "revision": "46abbc4a676739dbd61f8a305cb63fd8",
     "url": "/img/nucleo-icons.46abbc4a.svg"
-  },
-  {
-    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
-    "url": "/fonts/nucleo-icons.2569aaea.woff"
-  },
-  {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
-  },
-  {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
-  },
-  {
-    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
-    "url": "/img/fontawesome-webfont.acf3dcb7.svg"
   },
   {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
     "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
   },
   {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
   },
   {
-    "revision": "30e339d7e8bf7114352d028f6435cd19",
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "8c9d1d633fab6ec1de731104eb3bb070",
     "url": "/index.html"
   },
   {
@@ -72,28 +72,24 @@ self.__precacheManifest = [
     "url": "/favicon.png"
   },
   {
-    "revision": "2438aec598879be1f8ee90f1f9eb2bdd",
-    "url": "/img/brand/blue.png"
-  },
-  {
     "revision": "fe4792d482196a50cf9ae0d9d90b6493",
     "url": "/img/backup favicon (2).png"
-  },
-  {
-    "revision": "185288d13ed8e9d745bd279ea34667bf",
-    "url": "/img/brand/blue BACKUP.png"
   },
   {
     "revision": "c85c75275c0a0a617f9e5accc2700908",
     "url": "/img/brand/creativetim-white-slim.png"
   },
   {
-    "revision": "7c73456e8eade4cf9c20219efc1891b4",
-    "url": "/img/brand/favicon.png"
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue BACKUP.png"
   },
   {
-    "revision": "be671d3c1486132f5bfbc9b9f2e6fad9",
-    "url": "/img/brand/white.png"
+    "revision": "2438aec598879be1f8ee90f1f9eb2bdd",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "7c73456e8eade4cf9c20219efc1891b4",
+    "url": "/img/brand/favicon.png"
   },
   {
     "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
@@ -104,12 +100,16 @@ self.__precacheManifest = [
     "url": "/img/brand/white backup.png"
   },
   {
+    "revision": "be671d3c1486132f5bfbc9b9f2e6fad9",
+    "url": "/img/brand/white.png"
+  },
+  {
     "revision": "594b1ee1d95ada356eaad078e9217932",
     "url": "/img/ill/ill-2.svg"
   },
   {
-    "revision": "1cab885a63a77ff5bbf1cd9881850eb0",
-    "url": "/img/theme/img-1-1200x1000.jpg"
+    "revision": "a87f4e3c160e2ee57749f336a85abaa9",
+    "url": "/img/theme/img-2-1200x1000.jpg"
   },
   {
     "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
@@ -120,8 +120,12 @@ self.__precacheManifest = [
     "url": "/img/theme/img-2-1200x1000 backup.jpg"
   },
   {
-    "revision": "a87f4e3c160e2ee57749f336a85abaa9",
-    "url": "/img/theme/img-2-1200x1000.jpg"
+    "revision": "1cab885a63a77ff5bbf1cd9881850eb0",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "fde769aa06bf113884a43e8ca6f890d7",
+    "url": "/img/theme/img-3-1200x1000.jpg"
   },
   {
     "revision": "dc49ad52655e1d9d0552c026db3ef688",
@@ -132,24 +136,20 @@ self.__precacheManifest = [
     "url": "/img/theme/profile.jpg"
   },
   {
-    "revision": "beca74e2eeaa267f0b757b2afd4fa3b4",
-    "url": "/img/theme/img-5-1200x1000.jpg"
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
   },
   {
     "revision": "15449a9e7f6198f59736aafb440208c5",
     "url": "/img/theme/img-4-1200x1000.jpg"
   },
   {
-    "revision": "974088a1931e40895bac6db119c62448",
-    "url": "/img/theme/promo-1.png"
+    "revision": "beca74e2eeaa267f0b757b2afd4fa3b4",
+    "url": "/img/theme/img-5-1200x1000.jpg"
   },
   {
     "revision": "edc7106b21ec12e57022b2ebd534cd2d",
     "url": "/img/theme/team-1-800x800.jpg"
-  },
-  {
-    "revision": "fde769aa06bf113884a43e8ca6f890d7",
-    "url": "/img/theme/img-3-1200x1000.jpg"
   },
   {
     "revision": "be997d5226b992ffad34816870c6b7aa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48ab6c01d3daef025c7676555857aec8.js"
+  "/precache-manifest.5aac797cf9d6e84cc7cc8c816cb6ca44.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "PMII-Sepuluh-Nopember"});
