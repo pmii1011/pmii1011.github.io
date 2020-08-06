49d37c39c461e88e45dadbc1fc8236aa517f993e
ikmalil birri <malikmal98@gmail.com>
2020-08-07 04:09:30 +0700

    progress deploy  homepage landing page

--- README.md
+++
@@ -1,9 +0,0 @@
-# pmii1011.github.io
-Organization Profile
-
-
-
-<h1>Wellcome to PMII Sepuluh Nopember</h1>
-<pre>Website dalam pengembangan</pre>
-
-<a href="https://pmii1011.github.io/" >https://pmii1011.github.io</a> 
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.de2b40832e62b4f01567cc3a70034337.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "PMII-Sepuluh-Nopember"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
