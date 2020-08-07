d2b55c9c09a3e3c5dcde483d02d1cdf27134e8b9
ikmalil birri <malikmal98@gmail.com>
2020-08-07 12:54:28 +0700

    progress add duta pmii sepuluh nopember"

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5aac797cf9d6e84cc7cc8c816cb6ca44.js"
+  "/precache-manifest.2849b6ad406ef906bd67f4c606fd0a72.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "PMII-Sepuluh-Nopember"});
